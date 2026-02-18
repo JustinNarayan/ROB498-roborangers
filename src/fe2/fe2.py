524ab99e8cb2ae7422c7b2a971e06756a4dba531
Justin Narayan <justin.narayan@mail.utoronto.ca>
2026-02-18 14:39:13 -0500

    Launch infra

--- src/fe2/fe2.py
+++ src/fe2/fe2.py
@@ -34,11 +34,11 @@ def callback_abort(request, response):
 
 class CommNode(Node):
     def __init__(self):
-        super().__init__('rob498_drone_XX')
-        self.srv_launch = self.create_service(Trigger, 'rob498_drone_XX/comm/launch', callback_launch)
-        self.srv_test = self.create_service(Trigger, 'rob498_drone_XX/comm/test', callback_test)
-        self.srv_land = self.create_service(Trigger, 'rob498_drone_XX/comm/land', callback_land)
-        self.srv_abort = self.create_service(Trigger, 'rob498_drone_XX/comm/abort', callback_abort)
+        super().__init__('rob498_drone_06')
+        self.srv_launch = self.create_service(Trigger, 'rob498_drone_06/comm/launch', callback_launch)
+        self.srv_test = self.create_service(Trigger, 'rob498_drone_06/comm/test', callback_test)
+        self.srv_land = self.create_service(Trigger, 'rob498_drone_06/comm/land', callback_land)
+        self.srv_abort = self.create_service(Trigger, 'rob498_drone_06/comm/abort', callback_abort)
 
 def main(args=None):
     rclpy.init(args=args)
